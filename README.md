884a190dd657cf1d2d4099ce5bd95570a91aa10f
Brian Mason <brian.dale.mason@gmail.com>
2017-06-07 21:19:38 -0700

    Set up basic installation of packages, config
    This commit sets up a very bare-bones install script with a few homebrew
    packages and symlinks some basic vim and git config. Some of the
    included vim config will be superceded by fugitive.vim, once vim
    packages have been added.
    
    The code here has been verified to work against a clean install of macOS
    Sierra, but nothing here has been tested thoroughly, and would not be
    guaranteed to work in an existing environment.

---
+++ README.md
@@ -0,0 +1,14 @@
+# README
+
+## Setup
+
+Run ./setup.sh from this directory.
+
+## Manual Steps
+
+- set global git user name and email:
+```
+git config --global user.name "..."
+git config --global user.email "..."
+```
+- 
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Step 0: Update Some Global Config
+
+# Set fastest key repeat
+defaults write -g InitialKeyRepeat -int 15 
+defaults write -g KeyRepeat -int 2 
+
+# Step 1: Install Homebrew
+if hash brew 2>/dev/null; then
+  echo "brew already installed, proceeding with setup."
+else
+  /usr/bin/ruby -e \
+  "$(curl -fsSL \
+  https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Step 2: Install Homebrew Packages
+cat ./brew_packages | xargs -o brew install
+
+# Step 3: Install Homebrew Casks
+cat ./brew_casks | xargs -o brew cask install
+
+# Step 4: Symlink config files
+./setup/symlink.sh

6adc25cf5893768e5b7e085a72f84948e199dedc
Brian Mason <brian.dale.mason@gmail.com>
2017-06-10 15:48:47 -0700

    Skip already installed homebrew casks

--- setup/brew_casks.sh
+++ setup/brew_casks.sh
@@ -3,4 +3,14 @@
 # TODO: This is brittle since this script is in a different location from where
 # it presumes it is being called, this should be updated to work based on the
 # relative positions of this script and the cask list.
-cat ./brew_casks | xargs -o brew cask install
+grep -v '^$' ./brew_casks | while read cask; do
+  if [ -z "$cask" ]; then
+    continue
+  fi
+
+  if brew cask ls --versions $cask > /dev/null; then
+    echo "$cask already isntalled, skipping." && continue
+  fi
+
+  brew cask install $cask
+done

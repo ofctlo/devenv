4355f36619c3c1d05d788cf3bf2928c7a17f0870
Brian Mason <brian.dale.mason@gmail.com>
2017-06-10 17:32:56 -0700

    Revert "Don't attempt to install installed brew packages"
    This change leads to "stdin is not a terminal".

--- setup/brew_packages.sh
+++ setup/brew_packages.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-grep -v '^$' ./brew_packages | while read package; do
-  if [ -z "$package" ]; then
-    continue
-  fi
-
-  if brew ls --versions $package > /dev/null; then
-    echo "$package already installed, skipping." && continue
-  fi
-
-  brew install $package
-done
+cat ./brew_packages | xargs -o brew install

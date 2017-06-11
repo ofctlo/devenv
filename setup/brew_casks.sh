4d877476ab316359c58530dc80dbe80636a2d7c6
Brian Mason <brian.dale.mason@gmail.com>
2017-06-10 17:33:38 -0700

    Revert "Skip already installed homebrew casks"
    This change was causing the error "stdin is not a terminal"

--- setup/brew_casks.sh
+++ setup/brew_casks.sh
@@ -3,14 +3,4 @@
 # TODO: This is brittle since this script is in a different location from where
 # it presumes it is being called, this should be updated to work based on the
 # relative positions of this script and the cask list.
-grep -v '^$' ./brew_casks | while read cask; do
-  if [ -z "$cask" ]; then
-    continue
-  fi
-
-  if brew cask ls --versions $cask > /dev/null; then
-    echo "$cask already isntalled, skipping." && continue
-  fi
-
-  brew cask install $cask
-done
+cat ./brew_casks | xargs -o brew cask install

38819de62c1f50a6c7ce1ec5219a25c1e58a42d4
Brian Mason <brian.dale.mason@gmail.com>
2017-06-18 09:21:27 -0700

    Setup tab navigation in Safari like Chrome

--- setup/plugins/macos/setup.sh
+++ setup/plugins/macos/setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Menubar clock
 defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  H:mm:ss"
 defaults write com.apple.menuextra.clock FlashDateSeparators NO
 defaults write com.apple.menuextra.clock IsAnalog NO
 
 killall -KILL SystemUIServer
+
+# Safari tab navigation
+defaults write com.apple.Safari NSUserKeyEquivalents \
+  -dict-add 'Show Previous Tab' -string '@$['
+defaults write com.apple.Safari NSUserKeyEquivalents \
+  -dict-add 'Show Next Tab' -string '@$]'
+
+if ( ! defaults read com.apple.universalaccess 'com.apple.custommenu.apps' | grep 'Safari' ); then
+  defaults write com.apple.universalaccess 'com.apple.custommenu.apps' \
+    -array-add 'com.apple.Safari'
+fi
